9e2c700d933d4ad5a4a9b30facfa3fb11fc193a8
DilysT <21522624@gm.uit.edu.vn>
2024-10-09 23:10:07 +0700

    UI testing

--- src/App.js
+++ src/App.js
@@ -1,19 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from './components/dashboard/Header';
 import SideBar from './components/dashboard/Sidebar';
 import Dashboard from './components/dashboard/Dashboard';
+import Onboard from './components/onboarding/Onboard';  // Import the Onboard component
 import './App.css';
 
-
 const App = () => {
+  // Add state to track whether the onboarding is completed
+  const [isOnboardingComplete, setIsOnboardingComplete] = useState(false);
+
+  // Function to handle onboarding completion
+  const completeOnboarding = () => {
+    setIsOnboardingComplete(true);
+  };
+
   return (
     <div className="app">
-      <SideBar />
-      <div className="main-content">
-        <Header />
-        <Dashboard />
-        
-      </div>
+      {/* Conditional rendering: show Onboard component if not completed, otherwise show Dashboard */}
+      {!isOnboardingComplete ? (
+        <Onboard onComplete={completeOnboarding} />  
+      ) : (
+        <>
+          <SideBar />
+          <div className="main-content">
+            <Header />
+            <Dashboard />
+          </div>
+        </>
+      )}
     </div>
   );
 };
